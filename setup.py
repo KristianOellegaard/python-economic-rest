dfa7e3a15b855eb8c713574c795ebd4f4c850257
Kristian Øllegaard <kristian@kristian.io>
2013-10-19 17:36:00 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+from economic import __version__
+
+setup(
+    name = "django-health-check",
+    version = __version__,
+    author = "Kristian Oellegaard",
+    author_email = "kristian@kristian.io",
+    description = "Easy to use and dynamic python interface to the experimental e-conomic REST api",
+    license = "BSD",
+    keywords = "economic e-conomic accounting bookkeeping",
+    url = "https://github.com/KristianOellegaard/python-economic-rest",
+    packages=['economic'],
+    classifiers=[
+    ],
+    install_requires=[
+    ],
+    include_package_data=True,
+    zip_safe = False,
+)
